1736eee53e1a1393cea35e2cc529f5fc9914896d
cmoutzou <cmoutzou@hotmail.com>
2023-01-18 21:31:10 +0200

    c

--- core/settings.py
+++ core/settings.py
@@ -141,11 +141,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
     }
 else:'''
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-    },
-	'default2':{
+	'default':{
 		'ENGINE': 'django.db.backends.mysql',
 		'NAME': 'biokliniki',
 		'USER'    : 'root',
