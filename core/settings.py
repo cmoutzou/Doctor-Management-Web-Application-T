df015398c37f416ce9f918ee4d4d9982e71c713c
cmoutzou <cmoutzou@hotmail.com>
2023-01-19 06:00:06 +0200

    c

--- core/settings.py
+++ core/settings.py
@@ -67,7 +67,7 @@ CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1', 'https://' + 
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    #'django.contrib.auth',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
